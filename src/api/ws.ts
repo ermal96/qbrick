e45a8a869f8f36aae1f82722b9b68cabab970b9c
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2022-03-17 15:20:01 +0100

    fix: add ws config

---
+++ src/api/ws.ts
@@ -0,0 +1,3 @@
+export const WS_URL = "wss://ws.finnhub.io";
+export const APPL_STOCK = JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'});
+export const BTCUSDT_STOCK = JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'})
