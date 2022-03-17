2dad7708a39978245aba3c07caef2a3d1bf85014
Ermal Vrapi <ermalvrapi18@gmail.com>
2022-03-17 23:53:19 +0100

    fix: get stock and add tooltip custom

--- src/components/Chart.tsx
+++ src/components/Chart.tsx
@@ -1,35 +1,86 @@
-import { LineChart, Line, YAxis, CartesianGrid, Tooltip } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 import styled from "styled-components";
 
 const ChartWrapper = styled.div`
-    display:flex;
-    align-items: center;
-    justify-content: center;
-    width: 100vw;
-    height: 100vh;
+    display:grid;
+    grid-template-columns: 1fr 3fr;
+    width: 100%;
+    min-height: 100vh;
 `;
 
-export type ChartProps = {
-    data: {
-        APPL: number,
-    }[]
+const CustomToolTip = styled.div`
+  background-color:  #ffffff;
+  box-shadow: 0px 0px 10px #dfdbdb;
+  border-radius: 8px;
+  padding: 5px 15px;
+`;
+
+const CustomToolTipItem = styled.p`
+  font-weight: 500;
+  font-size: 14px;
+`;
+
+type ChartData = {
+  PRICE: number,
+  TIME: string,
+  VOLUME: number
 }
 
-const Chart = ({data} : ChartProps) => {
+const Chart = ({stocks}: any) => {
+  const data: ChartData[] = [];
+
+  if(stocks) {
+    stocks.forEach((item: {
+      p: number,
+      t: number,
+      v: number
+    }) => {
+      data.push({
+        PRICE: item.p,
+        TIME: new Date(item.t).toISOString().split("T")[1].split(".")[0],
+        VOLUME: item.v
+      })
+    })
+  }
+
+  const CustomTooltip = ({ active, payload, label }: any) => {
+    if (active && payload && payload.length) {
+      return (
+        <CustomToolTip>
+          <CustomToolTipItem>{`Time: ${label}`}</CustomToolTipItem>
+          <CustomToolTipItem style={{color: "#4941eb"}}>{`Price : ${payload[0].value}`}</CustomToolTipItem>
+          <CustomToolTipItem style={{color: "#67ca8d"}}>{`Volume: ${payload[1].value}`}</CustomToolTipItem>
+        </CustomToolTip>
+      );
+    }
+  
+    return null;
+  };
+  
+
   return (
       <ChartWrapper>
-        <LineChart 
-            width={900} 
-            height={400} 
-            data={data}
-            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+        <div></div>
+       <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            bottom: 30,
+            left: 50,
+            right: 50,
+            top: 50
+          }}
         >
-            <CartesianGrid strokeDasharray="3 3" />
-            <YAxis />
-            <Tooltip />
-            <Line type="monotone" dataKey="BTCUSDT" stroke="#8884d8" />
-            <Line type="monotone" dataKey="APPL" stroke="#82ca9d" />
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="TIME" />
+          <YAxis />
+          <Tooltip content={<CustomTooltip />} />
+          <Line type="monotone" dataKey="PRICE" stroke="#4941eb" activeDot={{ r: 8 }} />
+          <Line type="monotone" dataKey="VOLUME" stroke="#67ca8d" />
         </LineChart>
+      </ResponsiveContainer>
       </ChartWrapper>
    
   )
