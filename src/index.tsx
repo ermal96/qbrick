30f5548c4a353d42ea956e051c8485619491a633
Ermal Vrapi <ermalvrapi18@gmail.com>
2022-03-17 23:52:23 +0100

    fix: import index css

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { store } from './store'
 import { Provider } from 'react-redux'
+import "./index.css";
+
 
 ReactDOM.render(
   <Provider store={store}>
