937998018e4eec9e0aa5fad0c787f03f7065cfcd
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2022-03-17 16:40:01 +0100

    fix: add stock slice

--- src/store/slices/stockSlice.ts
+++ src/store/slices/stockSlice.ts
@@ -1,12 +1,27 @@
 import { createSlice } from '@reduxjs/toolkit'
+import { setStock } from '../actions/stockActions';
 import { ChartState } from '../../types'
 
-const initialState: ChartState = {}
+const initialState: ChartState = {
+  status: "init",
+}
+
+const setStcokPending = (state: ChartState ) => {
+  state.status = "loading";
+}
+
+const setStcokFulfilled = (state: ChartState, action: any ) => {
+
+}
 
 export const chartSlice = createSlice({
   name: 'chart',
   initialState,
   reducers: {},
+  extraReducers: {
+    [setStock.pending as any]: setStcokPending,
+    [setStock.fulfilled as any]: setStcokFulfilled,
+}
 })
 
 export default chartSlice.reducer
