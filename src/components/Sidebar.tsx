7d746e62bc7d0791d7b0573ac5afcfd633bd9333
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2022-03-18 11:49:15 +0100

    fix: add sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,9 +1,71 @@
 import { SidebarProps } from "../types";
+import styled from "styled-components";
+
+const SideBar = styled.nav`
+  padding: 4rem;
+  background-image: linear-gradient( 112.1deg,  rgba(32,38,57,1) 11.4%, rgba(63,76,119,1) 70.2% );
+`;
+
+const Title = styled.h1`
+  font-size: 2.3rem;
+  color: white;
+`;
+const Subtitle = styled.p`
+  color: #4b4747;
+  font-size: 1.4rem;
+  color: white;
+`;
+
+const InfoBox = styled.div`
+  border: 1px solid;
+  margin-bottom: 3rem;
+  display:flex;
+  flex-direction: column;
+  padding: 1rem;
+  border-radius: 0.8rem;
+  span {
+    &:last-child {
+      margin-top: 1rem;
+      font-weight: bold;
+    }
+  }
+
+  &.time {
+    border-color: #a3a0f7;
+    color: #a3a0f7;
+  }
+  &.volume {
+    border-color: #67ca8d;
+    color: #67ca8d;
+  }
+  &.price {
+    border-color: #ffffff;
+    color: #ffffff;
+  }
+ 
+`;
 
 const Sidebar = ({data}: SidebarProps) => {
 
   return (
-    <div>Sidebar</div>
+    <SideBar>
+      <Title>APPLE STOCK</Title>
+      <Subtitle>AAPL</Subtitle>
+      <InfoBox className="time">
+          <span>Updated</span>
+          <span>{data.TIME}</span>
+      </InfoBox>
+
+      <InfoBox className="volume">
+          <span>Volume</span>
+          <span>{data.VOLUME}</span>
+      </InfoBox>
+
+      <InfoBox className="price">
+          <span>Price</span>
+          <span>{data.PRICE} $</span>
+      </InfoBox>
+    </SideBar>
   )
 }
 
