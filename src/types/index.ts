cf8bf552b4702bad3af8f78679016b38e92ac76e
Ermal Vrapi <ermal.vrapi@crispybacon.al>
2022-03-18 09:51:33 +0100

    fix: update types

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,21 @@ export type Message = {
     t: number[], // timestamp
     v: number[], // volume
 }
+
+export type StcokData = {
+    PRICE: number,
+    TIME: string,
+    VOLUME: number
+  }
+
+export type TooltipProps = {
+    active?: boolean,
+    payload?: {
+        value: string
+    }[],
+    label?: string,
+}
+
+export type SidebarProps = {
+    data: StcokData
+}
